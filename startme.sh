3aa5f54a9b7516fccc71c0603863a68c985888f2
René Bod <rene.bod@mac.com>
2024-06-10 22:19:26 +0200

    Cleanup

--- startme.sh
+++ startme.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
 docker compose up -d && docker compose logs -f
+# docker compose run --rm jupyter ansible-playbook -i hosts playbook.yml
+# docker compose run --rm jupyter ansible-playbook -i hosts site.yml --tags test_01
